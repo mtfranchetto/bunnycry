a91b6aa9cb9d3d37fccdf90610dbf692b0d86cdd
Mattia Franchetto <mt.franchetto@gmail.com>
2015-03-10 12:00:01 +0100

    Add test to event bus with different types of events

--- test/EventBus.js
+++ test/EventBus.js
@@ -19,4 +19,19 @@ describe('Event Bus', function () {
         expect(subscriber.action).toHaveBeenCalledWith({test: 10});
         expect(subscriber.action.calls.count()).toBe(1);
     });
+
+    it("should not receive the event if wrong type of event", function () {
+        var eventBus = new EventBus(),
+            subscriber = {
+                action: function (data) {
+
+                }
+            };
+        spyOn(subscriber, "action");
+
+        eventBus.subscribe("TEST_NOTIFICATION_WRONG", subscriber.action);
+        eventBus.publish("TEST_NOTIFICATION", {test: 10});
+
+        expect(subscriber.action.calls.count()).toBe(0);
+    });
 });

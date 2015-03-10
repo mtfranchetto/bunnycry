afdadad059326aaed72778882581eb955897ca43
Mattia Franchetto <mt.franchetto@gmail.com>
2015-03-10 10:54:27 +0100

    Add EventBus

---
+++ test/EventBus.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var EventBus = require('../lib/messages/EventBus');
+
+describe('Event Bus', function () {
+
+    it("should publish an event", function () {
+        var eventBus = new EventBus(),
+            subscriber = {
+                action: function (data) {
+
+                }
+            };
+        spyOn(subscriber, "action");
+
+        eventBus.subscribe("TEST_NOTIFICATION", subscriber.action);
+        eventBus.publish("TEST_NOTIFICATION", {test: 10});
+
+        expect(subscriber.action).toHaveBeenCalledWith({test: 10});
+        expect(subscriber.action.calls.count()).toBe(1);
+    });
+});

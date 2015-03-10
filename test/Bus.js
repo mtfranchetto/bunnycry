228fd9908754577907cfdacf57ba13ad9a7a52a3
Mattia Franchetto <mt.franchetto@gmail.com>
2015-03-10 14:35:53 +0100

    Move to spy manual creation

--- test/Bus.js
+++ test/Bus.js
@@ -2,36 +2,26 @@
 
 var Bus = require('../lib/messages/Bus');
 
-describe('Event Bus', function () {
+describe('Bus', function () {
 
     it("should publish an event", function () {
         var bus = new Bus(),
-            subscriber = {
-                action: function (data) {
+            spy = jasmine.createSpy();
 
-                }
-            };
-        spyOn(subscriber, "action");
-
-        bus.subscribe("TEST_NOTIFICATION", subscriber.action);
+        bus.subscribe("TEST_NOTIFICATION", spy);
         bus.publish("TEST_NOTIFICATION", {test: 10});
 
-        expect(subscriber.action).toHaveBeenCalledWith({test: 10});
-        expect(subscriber.action.calls.count()).toBe(1);
+        expect(spy).toHaveBeenCalledWith({test: 10});
+        expect(spy.calls.count()).toBe(1);
     });
 
     it("should not receive the event if wrong type of event", function () {
         var bus = new Bus(),
-            subscriber = {
-                action: function (data) {
+            spy = jasmine.createSpy();
 
-                }
-            };
-        spyOn(subscriber, "action");
-
-        bus.subscribe("TEST_NOTIFICATION_WRONG", subscriber.action);
+        bus.subscribe("TEST_NOTIFICATION_WRONG", spy);
         bus.publish("TEST_NOTIFICATION", {test: 10});
 
-        expect(subscriber.action.calls.count()).toBe(0);
+        expect(spy.calls.count()).toBe(0);
     });
 });

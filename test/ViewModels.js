e3e2e7ba81d46f0433433ebbc7719b77664a195e
Mattia Franchetto <mt.franchetto@gmail.com>
2015-03-11 12:28:20 +0100

    Register events listeners

--- test/ViewModels.js
+++ test/ViewModels.js
@@ -16,23 +16,27 @@ describe('ViewModels', function () {
     beforeEach(module('test'));
 
     var controllerProvider = null,
-        rootScope = null;
+        rootScope = null,
+        propertiesRegistry = null,
+        eventsRegistry = null;
 
     beforeEach(inject(function (_$controller_, _$rootScope_) {
         controllerProvider = _$controller_;
         rootScope = _$rootScope_;
+        propertiesRegistry = new Registry();
+        eventsRegistry = new Registry();
     }));
 
     it("should watch the property of a viewmodel", function () {
-        var propertyCommand = new Command(),
-            propertiesRegistry = new Registry();
+        var propertyCommand = new Command();
         propertiesRegistry.register('ViewModel', 'testProperty', propertyCommand);
         spyOn(propertyCommand, "execute");
         var scope = rootScope.$new(),
             viewmodel = controllerProvider('ViewModel', {
                 'context': 'ViewModel',
                 'scope': scope,
-                'propertiesRegistry': propertiesRegistry
+                'propertiesRegistry': propertiesRegistry,
+                'eventsRegistry': eventsRegistry
             });
 
         viewmodel.testProperty = 20;
@@ -40,5 +44,23 @@ describe('ViewModels', function () {
 
         expect(propertyCommand.execute).toHaveBeenCalledWith(20, 20);
         expect(propertyCommand.execute.calls.count()).toBe(1);
+    });
+
+    it("should listen to an emit of an event", function () {
+        var eventCommand = new Command();
+        eventsRegistry.register('ViewModel', 'testEvent', eventCommand);
+        spyOn(eventCommand, "execute");
+        var scope = rootScope.$new(),
+            viewmodel = controllerProvider('ViewModel', {
+                'context': 'ViewModel',
+                'scope': scope,
+                'propertiesRegistry': propertiesRegistry,
+                'eventsRegistry': eventsRegistry
+            });
+
+        scope.$emit('testEvent', {test: 10});
+
+        expect(eventCommand.execute).toHaveBeenCalledWith({ test: 10});
+        expect(eventCommand.execute.calls.count()).toBe(1);
     });
 });

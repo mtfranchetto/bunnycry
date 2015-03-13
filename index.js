e3a889bd030f0fcc4e6cbd818c2a194a8204f4fa
Mattia Franchetto <mt.franchetto@gmail.com>
2015-03-13 16:48:13 +0100

    Add index file

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 "use strict";
 
-module.exports = {};
+module.exports = {
+    "messages": {
+        "Bus": require('./lib/messages/Bus')
+    },
+    "rest": {
+        "InterfaceConstructor": require('./lib/rest/InterfaceConstructor'),
+        "RestAdapter": require('./lib/rest/RestAdapter')
+    },
+    "Command": require('./lib/Command'),
+    "ComposableViewModel": require('./lib/ComposableViewModel'),
+    "Registry": require('./lib/Registry'),
+    "ViewModel": require('./lib/ViewModel')
+};

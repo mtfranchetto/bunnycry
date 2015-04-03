d73c2140aeb2ed191320442d2bbe02b0dce32135
Mattia Franchetto <mt.franchetto@gmail.com>
2015-04-03 12:52:22 +0200

    Add composite subscription

--- test/Bus.js
+++ test/Bus.js
@@ -1,14 +1,16 @@
 "use strict";
 
-var Bus = require('../lib/messages/Bus');
+var Bus = require('../lib/messages/Bus'),
+    CompositeSubscription = require('../lib/messages/CompositeSubscription');
 
 describe('Bus', function () {
 
-    var bus, spy = null;
+    var bus, spy, compositeSubscription = null;
 
     beforeEach(function () {
         bus = new Bus();
         spy = jasmine.createSpy();
+        compositeSubscription = new CompositeSubscription();
     });
 
     it("should publish an event", function () {
@@ -31,5 +33,32 @@ describe('Bus', function () {
         bus.publish("context2", "TEST_NOTIFICATION", {test: 10});
 
         expect(spy.calls.count()).toBe(0);
+    });
+
+    it("should unsubscribe all the subscriptions", function () {
+        var subscription1 = bus.subscribe("context", "TEST_NOTIFICATION", spy),
+            subscription2 = bus.subscribe("context", "TEST_NOTIFICATION2", spy);
+        spyOn(subscription1, "unsubscribe");
+        spyOn(subscription2, "unsubscribe");
+        compositeSubscription.add(subscription1, subscription2);
+        compositeSubscription.unsubscribe();
+
+        expect(subscription1.unsubscribe.calls.count()).toBe(1);
+        expect(subscription2.unsubscribe.calls.count()).toBe(1);
+    });
+
+    it("should not dispose a subscription if it's a wrong object", function () {
+        var subscription1 = bus.subscribe("context", "TEST_NOTIFICATION", spy),
+            subscription2 = {
+                "foo": function () {
+                }
+            };
+        spyOn(subscription1, "unsubscribe");
+        spyOn(subscription2, "foo");
+        compositeSubscription.add(subscription1, subscription2);
+        compositeSubscription.unsubscribe();
+
+        expect(subscription1.unsubscribe.calls.count()).toBe(1);
+        expect(subscription2.foo.calls.count()).toBe(0);
     });
 });
